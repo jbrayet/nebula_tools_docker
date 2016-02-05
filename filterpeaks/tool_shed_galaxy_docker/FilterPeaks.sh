86b0c4cf36dad8a4463c8afa16ee6e52dcefc802
jocelyn brayet <jocelyn.brayet@curie.fr>
2016-02-05 17:03:55 +0100

    add peak2bed

--- filterpeaks/tool_shed_galaxy_docker/FilterPeaks.sh
+++ filterpeaks/tool_shed_galaxy_docker/FilterPeaks.sh
@@ -18,16 +18,16 @@ l) lenfile="$OPTARG";;
 esac
 done
 
-local_path=$(dirname $(readlink -f $0))
+local_path=/usr/bin/filterpeaks
 
 #echo "$@" > /data/tmp/tmp.log
-echo "java -classpath /bioinfo/http/prod/hosted/nebula.curie.fr/galaxy-dist/tools/curie_tools/peak_calling/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t $minHeight -v $minRatio -o $output" >> /dev/null
-java -classpath $local_path/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t $minHeight -v $minRatio -o $output > /dev/null
-java -classpath $local_path/ -Xmx6g FilterPeaks -c $inputfile -f $controlfile -t $minHeightControl -v $minRatio -o $outputControl > /dev/null
+echo "java -classpath local_path/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t $minHeight -v $minRatio -o $output"
+java -classpath $local_path/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t $minHeight -v $minRatio -o $output 
+java -classpath $local_path/ -Xmx6g FilterPeaks -c $inputfile -f $controlfile -t $minHeightControl -v $minRatio -o $outputControl
 
 if [ ! "$nameBed" == " " ]; then
-  perl $local_path/peak2bed.pl -f $output -t 0 -o $output.tmp -g $lenfile -n $nameBed > /dev/null
-  perl $local_path/peak2bed.pl -f $outputControl -t 0 -o $outputControl.tmp -g $lenfile -n $nameBedControl > /dev/null
+  perl $local_path/peak2bed.pl -f $output -t 0 -o $output.tmp -g $lenfile -n $nameBed
+  perl $local_path/peak2bed.pl -f $outputControl -t 0 -o $outputControl.tmp -g $lenfile -n $nameBedControl 
   cp $output.tmp $output
   rm $output.tmp
   cp $outputControl.tmp $outputControl
