61ef46095bbfdd1247a7341c63180b7ee4f477d0
jocelyn brayet <jocelyn.brayet@curie.fr>
2016-02-08 11:12:17 +0100

    add annotation files creation

--- filterpeaks/tool_shed_galaxy_docker/FilterPeaks.sh
+++ filterpeaks/tool_shed_galaxy_docker/FilterPeaks.sh
@@ -2,7 +2,7 @@
 
 nameBed=" "
 
-while getopts "f:c:t:v:o:m:w:n:q:l:" optionName; do
+while getopts "f:c:t:v:o:m:w:r:n::q::l::" optionName; do
 case "$optionName" in
 
 f) inputfile="$OPTARG";;
@@ -12,13 +12,19 @@ v) minRatio="$OPTARG";;
 o) output="$OPTARG";;
 m) minHeightControl="$OPTARG";;
 w) outputControl="$OPTARG";;
+r) ROOT_DIR="$OPTARG";;
 n) nameBed="$OPTARG";;
 q) nameBedControl="$OPTARG";;
-l) lenfile="$OPTARG";;
+l) BUILD="$OPTARG";;
 esac
 done
 
 local_path=/usr/bin/filterpeaks
+DATABASE_PATH=$ROOT_DIR/database/files
+mkdir -p $DATABASE_PATH/nebulaAnnotations
+mkdir -p $DATABASE_PATH/nebulaAnnotations/$BUILD
+nebulaAnnotationPath=$DATABASE_PATH/nebulaAnnotations
+nebulaGenomePath=$DATABASE_PATH/nebulaAnnotations/$BUILD
 
 #echo "$@" > /data/tmp/tmp.log
 echo "java -classpath local_path/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t $minHeight -v $minRatio -o $output"
@@ -26,8 +32,29 @@ java -classpath $local_path/ -Xmx6g FilterPeaks -f $inputfile -c $controlfile -t
 java -classpath $local_path/ -Xmx6g FilterPeaks -c $inputfile -f $controlfile -t $minHeightControl -v $minRatio -o $outputControl
 
 if [ ! "$nameBed" == " " ]; then
-  perl $local_path/peak2bed.pl -f $output -t 0 -o $output.tmp -g $lenfile -n $nameBed
-  perl $local_path/peak2bed.pl -f $outputControl -t 0 -o $outputControl.tmp -g $lenfile -n $nameBedControl 
+
+  ############### Create annotations files ################
+
+  FAIFILE='n'
+  LENFILE='n'
+  DICTFILE='n'
+  CHROFILE='n'
+  MAPFILE='n'
+
+  if [ ! -f $nebulaGenomePath/$BUILD.len ]; then
+      FAIFILE='y'
+      LENFILE='y'
+  fi
+  
+  chrom_info_file=$nebulaGenomePath/$BUILD.len
+  FILEPATH=$ROOT_DIR/tool-data
+  
+  bash /usr/bin/create_annotation_files.sh $FAIFILE $LENFILE $DICTFILE $CHROFILE $FILEPATH $BUILD $MAPFILE $nebulaGenomePath
+  
+  #################### END ANNOTATION FILES ###########################
+
+  perl $local_path/peak2bed.pl -f $output -t 0 -o $output.tmp -g $chrom_info_file -n $nameBed
+  perl $local_path/peak2bed.pl -f $outputControl -t 0 -o $outputControl.tmp -g $chrom_info_file -n $nameBedControl 
   cp $output.tmp $output
   rm $output.tmp
   cp $outputControl.tmp $outputControl
